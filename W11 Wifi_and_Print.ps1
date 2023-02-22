0f1e6fc3978073af1a182236d7d499ff15ed3101
cmacd88 <71598281+cmacd88@users.noreply.github.com>
2023-02-22 15:39:59 +0100

    Update W11 Wifi_and_Print.ps1
    Signed-off-by: cmacd88 <71598281+cmacd88@users.noreply.github.com>

--- W11 Wifi_and_Print.ps1
+++ W11 Wifi_and_Print.ps1
@@ -1,30 +1,76 @@
-Start-Transcript
+# Enable transcript logging
+Start-Transcript -Path C:\Temp\reg_log.txt
 
-$Errors = 0;
-
-$ComputerInfo = Get-ComputerInfo
-if(-not ($ComputerInfo.WindowsProductName -like "*Windows 11*")){
-    Write-Host 'This script only applies to Windows 11. You are running' $ComputerInfo.WindowsProductName
-    return 0
+# Check if OS is Windows 11
+if ([System.Environment]::OSVersion.Version.Build -lt 22000) {
+    Write-Verbose "This script is only compatible with Windows 11."
+    Stop-Transcript
+    exit 1
 }
 
-if(-not(Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Services\LanmanServer\Parameters" -Name "SMB1" -Value 1 -Force)){
-    Write-Host 'Unable to set LanManServer\SMB1 parameter.'
-    $Errors += 1
+# Define registry keys to set
+$registryKeys = @(
+    @{
+        Path = "HKLM:\System\CurrentControlSet\Services\LanmanServer\Parameters"
+        Name = "SMB1"
+        Value = 1
+        Type = "DWORD"
+    },
+    @{
+        Path = "HKLM:\System\CurrentControlSet\Control\Lsa"
+        Name = "LsaCfgFlags"
+        Value = 1
+        Type = "DWORD"
+    },
+    @{
+        Path = "HKLM:\Software\Policies\Microsoft\Windows\DeviceGuard"
+        Name = "LsaCfgFlags"
+        Value = 1
+        Type = "DWORD"
+    }
+)
+
+# Loop through registry keys and set values
+$errorCount = 0
+
+foreach ($key in $registryKeys) {
+    if (!(Test-Path $key.Path)) {
+        try {
+            New-Item -Path $key.Path -Force | Out-Null
+        }
+        catch {
+            Write-Error "Failed to create registry key $($key.Path): $_"
+            $errorCount++
+            continue
+        }
+    }
+
+    try {
+        $currentValue = (Get-ItemProperty $key.Path -ErrorAction Stop).$($key.Name)
+    }
+    catch {
+        Write-Error "Failed to read registry value $($key.Path)\$($key.Name): $_"
+        $errorCount++
+        continue
+    }
+
+    if ($currentValue -ne $key.Value) {
+        try {
+            New-ItemProperty -Path $key.Path -Name $key.Name -Value $key.Value -PropertyType $key.Type -Force | Out-Null
+        }
+        catch {
+            Write-Error "Failed to set registry value $($key.Path)\$($key.Name) to $($key.Value): $_"
+            $errorCount++
+            continue
+        }
+    }
 }
 
-if(-not(Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Lsa" -Name "LsaCfgFlags" -Value 1 -Force)){
-    Write-Host 'Unable to set Lsa\LsaCfgFlags parameter.'
-    $Errors += 1
+# Check if there were any errors and return appropriate exit code
+if ($errorCount -gt 0) {
+    Stop-Transcript
+    exit 1
 }
 
-if(-not(Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\DeviceGuard" -Name "LsaCfgFlags" -Value 1 -Force)){
-    Write-Host 'Unable to set DeviceGuard\LsaCfgFlags parameter.'
-    $Errors += 1
-}
-
-if(errors -eq 0){
-    return 0
-}
-
-return 1
+Stop-Transcript
+exit 0
